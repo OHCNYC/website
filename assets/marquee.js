cec5c09bc9849251991c2b90c4ae86a00bf11efb
lunaturing <luna.turing@mailbox.org>
2024-04-12 20:02:07 -0400

    made better

---
+++ assets/marquee.js
@@ -0,0 +1,20 @@
+function Marquee() {
+    const parentSelector = document.querySelector(".marquee");
+    console.log(parentSelector);
+    const clone = parentSelector.innerHTML;
+    const firstElement = parentSelector.children[0];
+    let i = 0;
+    console.log(firstElement);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+
+    setInterval(function() {
+        firstElement.style.marginLeft = `-${i}px`;
+        if (i > firstElement.clientWidth) {
+            i = 0;
+        }
+        i = i + 0.2;
+    }, 0);
+}
+
+window.addEventListener('load', Marquee)
